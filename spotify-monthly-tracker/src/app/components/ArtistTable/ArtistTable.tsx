f9581510bfd70dcac15671fbc81e8c51251b00ce
wnick1678 <w.nick1678@gmail.com>
2025-02-01 13:54:14 -0500

    Implemented Artist Table component

---
+++ spotify-monthly-tracker/src/app/components/ArtistTable/ArtistTable.tsx
@@ -0,0 +1,20 @@
+import kekw from "./kekw.jpg";
+
+export default function ArtistTable() { 
+    const artistObj:any = []
+
+    const populateArtist = () => {
+        for (let i=1; i<6; i++){
+            artistObj.push(<li><img src={kekw.src} alt="loadFailed"/>Artist Name: {i}</li>)
+        }
+    }
+    populateArtist()
+
+    return (
+        <div>
+          <ul className='joe'>
+            {artistObj}
+          </ul>
+        </div>
+      )
+}
--- spotify-monthly-tracker/src/app/page.tsx
+++ spotify-monthly-tracker/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import dog from "./favicon.png"
 import GenreTable from "./components/GenreTable/GenreTable";
+import ArtistTable from "./components/ArtistTable/ArtistTable";
 
 export default function Home() {
   return (
     <div>
       <img src={dog.src} alt="loadFailed" />
       <GenreTable />
+      <ArtistTable />
     </div>
   );
 }
