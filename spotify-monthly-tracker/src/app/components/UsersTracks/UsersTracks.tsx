e2f1654a2d09867f1cb878037eb1c49efa872cb5
nakraathly <98604951+nakraathly@users.noreply.github.com>
2025-02-01 19:23:22 +0000

    Updated UserTracks variable

--- spotify-monthly-tracker/src/app/components/UsersTracks/UsersTracks.tsx
+++ spotify-monthly-tracker/src/app/components/UsersTracks/UsersTracks.tsx
@@ -1,28 +1,31 @@
 export default function UserTracks() {
     const TrackObj:any = []
-          
+    const TrackPic:any = []
+
+    const populatePicture = () => {
+      for (let i=1; i < 6; i++){
+        TrackPic.push(<li>Picture {i}</li>)
+      }
+    } 
+    
     const populateTracks = () => {
       for (let i=1; i < 6; i++){
         TrackObj.push(<li>Tracks {i}</li>)
       }
-    } 
-  
+    }
+
+    populatePicture()
     populateTracks()
-  
     return (
       <div className="flex"> 
-        <div>
         <ul className='joe'> 
           <div>Profile Pic </div>
-          {TrackObj}
+          {TrackPic}
         </ul>
-        </div>
-        <div>
         <ul className='joe'>
-          <div>{" "}Song Title</div>
+          <div>Song Title</div>
           {TrackObj}
         </ul>
-        </div>
       </div>
     )
   }
