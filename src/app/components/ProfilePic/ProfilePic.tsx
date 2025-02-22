474d64830d06accaea870b6fe33116b558ce67e4
wnick1678 <w.nick1678@gmail.com>
2025-02-22 14:43:54 -0500

    Updated the profile pic component to get profile picture from API

--- src/app/components/ProfilePic/ProfilePic.tsx
+++ src/app/components/ProfilePic/ProfilePic.tsx
@@ -1,18 +1,20 @@
-import dog from "../../favicon.png"
+"use client"
 
 export default function ProfilePic() {
     const profileObj:any = []
+    const profile:any = sessionStorage.getItem("Spotify-Profile");
 
     const populateProfilePic = () => {
-    profileObj.push(<img src={dog.src} alt="loadFailed" className="rounded-full w-80 h-80"/>)
-    }
+        const picture = JSON.parse(profile).images[0].url as string;
+        profileObj.push(<img src={picture} alt="loadFailed" className="rounded-full w-80 h-80"/>)
+        }
 
-
-populateProfilePic()
+        populateProfilePic()
 
 return (
     <div className = "flex justify-center">
         {profileObj}
     </div>
-  )
+    )
 }
+
