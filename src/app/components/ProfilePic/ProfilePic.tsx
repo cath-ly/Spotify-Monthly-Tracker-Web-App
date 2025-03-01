894a739847fa1aa0c638334ebbdb7f92a962c94b
wnick1678 <w.nick1678@gmail.com>
2025-03-01 15:10:27 -0500

    Updated Profile Picture component and added loading gif as placeholder

--- src/app/components/ProfilePic/ProfilePic.tsx
+++ src/app/components/ProfilePic/ProfilePic.tsx
@@ -1,15 +1,21 @@
 "use client"
-
+import dog from "../../favicon.png"
+import loading from "../../loading.gif"
 export default function ProfilePic() {
     const profileObj:any = []
     const profile:any = sessionStorage.getItem("Spotify-Profile");
 
     const populateProfilePic = () => {
         const picture = JSON.parse(profile).images[0].url as string;
+        if (profile == null) {
+            profileObj.push(<img src={loading.src} alt="loadFailed" className="rounded-full w-80 h-80"/>)
+        }
+        else {
         profileObj.push(<img src={picture} alt="loadFailed" className="rounded-full w-80 h-80"/>)
         }
+    }
 
-        populateProfilePic()
+    populateProfilePic();
 
 return (
     <div className = "flex justify-center">
@@ -17,4 +23,3 @@ return (
     </div>
     )
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-'use-client'
+'use client'
 
 import Image from "next/image";
 import dog from "./favicon.png";
+import loading from "./loading.gif"
+import React, { useState, useEffect } from "react";
 import GenreTable from "./components/GenreTable/GenreTable";
 import UserTracks from "./components/UsersTracks/UsersTracks"; 
 import ArtistTable from "./components/ArtistTable/ArtistTable";
@@ -12,11 +14,21 @@ import { SpotifyApiComponent } from "@/script";
 require('dotenv').config();
 
 export default function Home() {
+  const [showPfp, setShowPfp] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPfp(true);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [])
 
   return (
     <div>
       <SpotifyApiComponent/>
-        <ProfilePic />
+      <div className = "flex justify-center">
+       {showPfp ? <ProfilePic /> : <img src={loading.src} alt="loadFailed" className="rounded-full w-80 h-80"/>}
+      </div>
       <div className="flex justify-around mt-12">
         <GenreTable />
         <UserTracks/>
