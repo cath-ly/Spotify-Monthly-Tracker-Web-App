869ff140fbd45f2dfea9981cd90ed0d27b5cec3d
cath-ly <takath339@gmail.com>
2025-03-16 14:52:48 -0400

    Implemented UsersTracks

--- src/app/components/UsersTracks/UsersTracks.tsx
+++ src/app/components/UsersTracks/UsersTracks.tsx
@@ -1,30 +1,29 @@
+'use client'
+
 export default function UserTracks() {
+    const topTracks = sessionStorage.getItem("Spotify-Tracks");
     const TrackObj:any = []
     const TrackPic:any = []
 
-    const populatePicture = () => {
-      for (let i=1; i < 6; i++){
-        TrackPic.push(<li>Picture {i}</li>)
+    const populateObj = () => {
+      if (topTracks === null){
+        for (let i=1; i < 6; i++){
+          TrackPic.push(<li>Picture {i}</li>)
+          TrackPic.push(<li>Tracks {i}</li>)
+        }
+      } else {
+        const topTrackObj = JSON.parse(topTracks);
+        for (const topTrackAlbum of topTrackObj.items){
+          TrackPic.push(<li id={topTrackAlbum.uri}><img src={topTrackAlbum.album.images[2].url} alt={topTrackAlbum.album.name} /> {topTrackAlbum.name}</li>)
+        }
       }
     } 
-    
-    const populateTracks = () => {
-      for (let i=1; i < 6; i++){
-        TrackObj.push(<li>Tracks {i}</li>)
-      }
-    }
 
-    populatePicture()
-    populateTracks()
+    populateObj()
     return (
       <div className="flex gap-8"> 
-        <ul className='joe'> 
-          <div>Profile Pic </div>
+        <ul className='flex-col center'>
           {TrackPic}
-        </ul>
-        <ul className='joe'>
-          <div>Song Title</div>
-          {TrackObj}
         </ul>
       </div>
     )
