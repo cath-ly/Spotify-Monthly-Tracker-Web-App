66ae58bd55ec0f373ff501ffeb73b5fd4ded11df
cath-ly <takath339@gmail.com>
2025-03-23 16:28:44 -0400

    Resolved state issue and implemented Try & Catch onto fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,57 @@
-'use client'
+"use client";
 
-import Image from "next/image";
-import dog from "./favicon.png";
-import loading from "./loading.gif"
 import React, { useState, useEffect } from "react";
 import GenreTable from "./components/GenreTable/GenreTable";
-import UserTracks from "./components/UsersTracks/UsersTracks"; 
+import UserTracks from "./components/UsersTracks/UsersTracks";
 import ArtistTable from "./components/ArtistTable/ArtistTable";
 import ProfilePic from "./components/ProfilePic/ProfilePic";
 import { getSpotifyAPI } from "./utils/getSpotifyAPI";
-import { SpotifyApiComponent } from "@/script";
-
-require('dotenv').config();
+import {
+  SpotifyApiComponent,
+  fetchProfile,
+  getAccessToken,
+  redirectToAuthCodeFlow,
+} from "@/script";
 
 export default function Home() {
-  const [showPfp, setShowPfp] = useState(false);
+  const [profile, setProfile] = useState(null);
+  let code: string | null = null;
+  if (typeof window !== "undefined") {
+    const params = new URLSearchParams(window.location.search);
+    code = params.get("code");
+  }
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowPfp(true);
-    }, 300);
-    return () => clearTimeout(timer);
-  }, [])
+    const getSpotifyAuth = async (code: string | null) => {
+      if (!code) {
+        redirectToAuthCodeFlow("bbd6d5333456415ca8bad1bce919efad");
+      } else {
+        try {
+          const accessToken = await getAccessToken(
+            "bbd6d5333456415ca8bad1bce919efad",
+            code
+          );
+          const data = await fetchProfile(accessToken);
+          console.log(data);
+          const picture = data.images[0].url;
+          setProfile(picture);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+          redirectToAuthCodeFlow("bbd6d5333456415ca8bad1bce919efad");
+        }
+      }
+    };
+    getSpotifyAuth(code);
+  }, []);
 
   return (
     <div>
-      <SpotifyApiComponent/>
-      <div className = "flex justify-center">
-       {showPfp ? <ProfilePic /> : <img src={loading.src} alt="loadFailed" className="rounded-full w-80 h-80"/>}
+      <div className="flex justify-center">
+        <ProfilePic pictureInfo={profile} />
       </div>
       <div className="flex justify-around mt-12">
         <GenreTable />
-        <UserTracks/>
+        <UserTracks />
       </div>
       <ArtistTable />
     </div>
