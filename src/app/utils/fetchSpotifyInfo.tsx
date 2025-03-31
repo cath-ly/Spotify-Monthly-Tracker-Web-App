2d51379fb585cb98da81bd0d8eb9b863c7f1ce0c
cath-ly <takath339@gmail.com>
2025-03-30 22:25:41 -0400

    Refactored fetchTopTracks

--- src/app/utils/fetchSpotifyInfo.tsx
+++ src/app/utils/fetchSpotifyInfo.tsx
@@ -1,8 +1,19 @@
+import { profileURL, topTracksURL } from "./fetchURL";
+
 export async function fetchProfile(token: string): Promise<any> {
-  const result = await fetch("https://api.spotify.com/v1/me", {
+  const result = await fetch(profileURL, {
     method: "GET",
     headers: { Authorization: `Bearer ${token}` },
   });
 
   return await result.json();
+}
+
+export async function fetchTopTracks(token: string): Promise<any> {
+  const topTracksResult = await fetch(topTracksURL, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return topTracksResult.json();
 }
