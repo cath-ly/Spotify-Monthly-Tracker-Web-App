50fee32d46c5f94cd3733ba65338d8aabdeb17f9
cath-ly <takath339@gmail.com>
2025-03-30 22:25:24 -0400

    Adjusted files to function after merge

--- src/app/components/UsersTracks/UsersTracks.tsx
+++ src/app/components/UsersTracks/UsersTracks.tsx
@@ -1,38 +1,47 @@
-'use client'
+"use client";
 
 import { ReactElement } from "react";
 
-export default function UserTracks(): ReactElement {
-    const topTracks: string | null = sessionStorage.getItem("Spotify-Tracks");
-    const trackPic: ReactElement[] = []
+export default function UserTracks(topTracks: any): ReactElement {
+  const trackPic: ReactElement[] = [];
+  console.log(topTracks);
 
-    const populateObj = () => {
-      if (topTracks == null){
-        for (let i=1; i < 6; i++){
-          trackPic.push(<li id={"Placeholder: " + i}>Picture {i} Tracks {i}</li>)
-        }
-      } else {
-        const topTrackObj = JSON.parse(topTracks);
-        for (const topTrackAlbum of topTrackObj.items){
-          trackPic.push(<li className="flex flex-row gap-8 mb-8" id={topTrackAlbum.uri}>
-            <img className="w-48 h-48" src={topTrackAlbum.album.images[1].url} alt={topTrackAlbum.album.name} />
-            <div className="flex flex-col justify-center w-100"> 
+  const populateObj = () => {
+    if (topTracks == null) {
+      for (let i = 1; i < 6; i++) {
+        trackPic.push(
+          <li id={"Placeholder: " + i}>
+            Picture {i} Tracks {i}
+          </li>
+        );
+      }
+    } else {
+      const topTrackObj = topTracks;
+      for (const topTrackAlbum of topTrackObj.topTracks) {
+        trackPic.push(
+          <li className="flex flex-row gap-8 mb-8" id={topTrackAlbum.uri}>
+            <img
+              className="w-48 h-48"
+              src={topTrackAlbum.album.images[1].url}
+              alt={topTrackAlbum.album.name}
+            />
+            <div className="flex flex-col justify-center w-100">
               <p className="font-sans text-2xl">{topTrackAlbum.name}</p>
               <p className="font-sans text-2xl">{topTrackAlbum.album.name}</p>
-              <p className="font-sans text-2xl">{topTrackAlbum.artists[0].name}</p>
-              </div>
-          </li>)
-        }
+              <p className="font-sans text-2xl">
+                {topTrackAlbum.artists[0].name}
+              </p>
+            </div>
+          </li>
+        );
       }
-    } 
+    }
+  };
 
-    populateObj()
-    return (
-      <div className="flex gap-8"> 
-        <ul className='flex-col'>
-          {trackPic}
-        </ul>
-      </div>
-    )
-  }
-  
+  populateObj();
+  return (
+    <div className="flex gap-8">
+      <ul className="flex-col">{trackPic}</ul>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { redirectToAuthCodeFlow } from "./utils/redirectToAuthCodeFlow";
 import { getAccessToken } from "./utils/getAccessToken";
-import { fetchProfile } from "./utils/fetchSpotifyInfo";
+import { fetchProfile, fetchTopTracks } from "./utils/fetchSpotifyInfo";
 import GenreTable from "./components/GenreTable/GenreTable";
 import UserTracks from "./components/UsersTracks/UsersTracks";
 import ArtistTable from "./components/ArtistTable/ArtistTable";
@@ -11,6 +11,7 @@ import ProfilePic from "./components/ProfilePic/ProfilePic";
 
 export default function Home() {
   const [picture, setPicture] = useState(null);
+  const [tracks, setTracks] = useState(null);
   let code: string | null = null;
   if (typeof window !== "undefined") {
     const params = new URLSearchParams(window.location.search);
@@ -27,9 +28,10 @@ export default function Home() {
             process.env.NEXT_PUBLIC_USER_ID,
             code
           );
-          const data = await fetchProfile(accessToken);
-          console.log(data);
-          setPicture(data.images[0].url);
+          const profile = await fetchProfile(accessToken);
+          const topTracks = await fetchTopTracks(accessToken);
+          setPicture(profile.images[0].url);
+          setTracks(topTracks.items);
         } catch (error) {
           console.error("Error fetching data:", error);
           redirectToAuthCodeFlow(process.env.NEXT_PUBLIC_USER_ID);
@@ -43,7 +45,7 @@ export default function Home() {
       <ProfilePic pictureInfo={picture} />
       <div className="flex justify-around mt-12">
         <GenreTable />
-        <UserTracks />
+        <UserTracks topTracks={tracks} />
       </div>
       <ArtistTable />
     </div>
